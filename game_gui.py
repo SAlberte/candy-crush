1f9c08cb07559f59a13c9212272087b3c8db4e0a
Albert <albi@onet.eu>
2020-06-01 21:49:26 +0200

    add saving current play to xml file add function that can show replay of last play

--- game_gui.py
+++ game_gui.py
@@ -2,7 +2,11 @@ from game_engine import Engine, Hexagon, Board
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
 from PySide2.QtCore import *
+import xml.etree.ElementTree as ET
+from xml.dom import minidom
+import codecs
 import threading
+import json
 import socket
 import pickle
 import os
@@ -26,12 +30,18 @@ class Window(QWidget):
         self.shouldcreateRoom = False
         self.is2PlayerMode = False
         self.wasFirstSettling = False
+        self.replayN = 0
+        self.replayActualN = 0
         self.shouldCloseSocket = False
         self.player1score = 0
         self.player2score = 0
+        self.IteratorTimer = 0
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.gameLoop)
         self.timer.setInterval(50)
+        self.replayTimer = QTimer(self)
+        self.replayTimer.timeout.connect(self.replayLoop)
+        self.replayTimer.setInterval(350)
         self.setTextBox()
         self.setLabel()
         self.setButtons()
@@ -56,6 +66,51 @@ class Window(QWidget):
         self.textbox.resize(100,40)
         self.textbox.setPlaceholderText(" IP ADDRESS")
 
+    def createXML(self):
+        # create the file structure
+        if self.replayN == 0:
+            data = ET.Element('data')
+            item0 = ET.SubElement(data, 'data')
+            item1 = ET.SubElement(data, 'item')
+            item1.set('object', str(self.replayN))
+            item1.text = codecs.encode(pickle.dumps(self.engine.board.hexagonslist), "base64").decode()
+
+            item2 = ET.SubElement(data, 'item1')
+            item2.set('score1', str(self.replayN))
+            item2.text = codecs.encode(pickle.dumps(self.player1score), "base64").decode()
+
+            item3 = ET.SubElement(data, 'item2')
+            item3.set('score2', str(self.replayN))
+            item3.text = codecs.encode(pickle.dumps(self.player2score), "base64").decode()
+
+            # create a new XML file with the results
+            mydata = ET.tostring(item0)
+            myfile = open("history.xml", "wb")
+            myfile.write(mydata)
+            self.replayN += 1
+        else:
+            tree = ET.parse('history.xml')
+            root = tree.getroot()
+
+            data = ET.Element('data')
+
+            item1 = ET.SubElement(data, 'item')
+            item1.set('object', str(self.replayN))
+            item1.text = codecs.encode(pickle.dumps(self.engine.board.hexagonslist), "base64").decode()
+
+            item2 = ET.SubElement(data, 'item1')
+            item2.set('score1', str(self.replayN))
+            item2.text = codecs.encode(pickle.dumps(self.player1score), "base64").decode()
+
+            item3 = ET.SubElement(data, 'item2')
+            item3.set('score2', str(self.replayN))
+            item3.text = codecs.encode(pickle.dumps(self.player2score), "base64").decode()
+
+            root.append(data)
+
+            tree.write('history.xml')
+            self.replayN += 1
+
     def setButtons(self):
         #button for 2 player mode
         self.btn2player = QPushButton("2 PLAYER MODE", self)
@@ -77,21 +132,76 @@ class Window(QWidget):
         self.btnstop.setStyleSheet("background-color: #66ffff")        
         self.btnstop.move(20, 150)
         self.btnstop.clicked.connect(self.quitApp)
-        
+
+        # button to get last ip addres
+        self.btnjoin = QPushButton("GET LAST IP ADDRESS", self)
+        self.btnjoin.setAutoDefault(False)
+        self.btnjoin.setStyleSheet("background-color: #66ffff")
+        self.btnjoin.move(20, 250)
+        self.btnjoin.clicked.connect(self.getIPaddr)
+
         #button to join online player
         self.btnjoin = QPushButton("JOIN PLAYER", self)
         self.btnjoin.setAutoDefault(False)
         self.btnjoin.setStyleSheet("background-color: #66ffff")
-        self.btnjoin.move(20, 250)
+        self.btnjoin.move(20, 300)
         self.btnjoin.clicked.connect(self.createEngineandJoin)
 
         # button to create room for online player
         self.btnjoin = QPushButton("CREATE ROOM", self)
         self.btnjoin.setAutoDefault(False)
         self.btnjoin.setStyleSheet("background-color: #66ffff")
-        self.btnjoin.move(20, 300)
+        self.btnjoin.move(20, 350)
         self.btnjoin.clicked.connect(self.createEngineAndMap)
-       
+
+        # button to play replay
+        self.btnjoin = QPushButton("PLAY REPLAY", self)
+        self.btnjoin.setAutoDefault(False)
+        self.btnjoin.setStyleSheet("background-color: #66ffff")
+        self.btnjoin.move(20, 400)
+        self.btnjoin.clicked.connect(self.playReplay)
+
+
+    def getIPaddr(self):
+        if os.path.exists("data.json"):
+            with open("data.json", "r") as read_file:
+                json_object = json.load(read_file)
+                self.textbox.setText(json_object["ip"])
+                return
+
+
+    def playReplay(self):
+        if os.path.exists("history.xml"):
+            self.isGameOnline = False
+            self.timer.stop()
+            self.engine = Engine()
+            self.replayActualN = 0
+            self.isGame = True
+            self.replayTimer.start()
+
+
+    def replayLoop(self):
+        if self.replayActualN == self.replayN-1:
+            print("koniec")
+            self.replayTimer.stop()
+            return
+        else:
+            #self.engine.board.hexagonlist = XXX
+            tree = ET.parse('history.xml')
+            root = tree.getroot()
+            n = root[self.replayActualN][0].text
+            score1 = root[self.replayActualN][1].text
+            score2 = root[self.replayActualN][2].text
+            n = pickle.loads(codecs.decode(n.encode(), "base64"))
+            score1 = pickle.loads(codecs.decode(score1.encode(),"base64"))
+            score2 = pickle.loads(codecs.decode(score2.encode(),"base64"))
+            print("pokazuje klatke nr"+ str(self.replayActualN))
+            self.engine.board.hexagonslist = n
+            self.replayActualN += 1
+            self.labelplayer1.setText(f"PLAYER 1 SCORE : {score1}   ")
+            self.labelplayer2.setText(f"PlAYER 2 SCORE : {score2}    ")
+            self.paint()
+
     def createRoom(self):
         self.isEnemyTurn = True
         self.t = threading.Thread(target=self.recieveBoard,args=(self.isEnemyTurn, self.engine.board.hexagonslist,))
@@ -105,13 +215,16 @@ class Window(QWidget):
         # Create a TCP/IP socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_address = (self.textbox.text(), 10061)
+        x = {
+            "ip": server_address[0],
+            "port": server_address[1],
+        }
+        with open('data.json', 'w') as outfile:
+            outfile.write(json.dumps(x))
 
         sock.connect(server_address)
 
         try:
-
-            # Send data
-            # message = b"This is our message. It is very long but will only be transmitted in chunks of 16 at a time"
             n = self.engine.board.hexagonslist
             message = pickle.dumps([n, self.player1score])
             # print('sending {!r}'.format(message))
@@ -166,17 +279,19 @@ class Window(QWidget):
             self.isEnemyTurn = False
             self.canIMove = True
 
+
     def quitApp(self):
         self.shouldCloseSocket = True
+        with open('data.json', 'w') as outfile:
+            outfile.write(json.dumps([ob.__dict__ for ob in self.engine.board.hexagonslist]))
         myApp.quit()
-
-
-
-
 
     def StartGame(self):
         self.canIMove = True
         self.shouldCloseSocket = True
+        self.replayTimer.stop()
+
+        self.replayN = 0
         self.labelplayer1.setText("PLAYER 1 SCORE : ")
         self.labelplayer2.setText("PLAYER 2 SCORE : ")
         self.isSettlingStart = False
@@ -197,7 +312,9 @@ class Window(QWidget):
         self.labelplayer1.setText("PLAYER 1 SCORE : ")
         self.labelplayer2.setText("PLAYER 2 SCORE : ")
         self.shouldCloseSocket = True
+        self.replayTimer.stop()
         self.isSettlingStart = False
+        self.replayN = 0
         self.shouldcreateRoom = False
         self.isGameOnline = False
         self.canIMove = True
@@ -215,6 +332,8 @@ class Window(QWidget):
         self.labelplayer1.setText("PLAYER 1 SCORE : ")
         self.labelplayer2.setText("PLAYER 2 SCORE : ")
         self.is2PlayerMode = False
+        self.replayTimer.stop()
+        self.replayN = 0
         self.shouldcreateRoom = False
         self.wasFirstSettling = False
         self.isSettlingStart = False
@@ -235,9 +354,11 @@ class Window(QWidget):
         self.labelplayer1.setText("PLAYER 1 SCORE : ")
         self.labelplayer2.setText("PLAYER 2 SCORE : ")
         self.isSettlingStart = False
+        self.replayN = 0
         self.shouldcreateRoom = False
         self.is2PlayerMode = False
         self.wasFirstSettling = False
+        self.replayTimer.stop()
         self.player1score = 0
         self.player2score = 0
         self.btnstart.setText("NEW GAME")
@@ -304,11 +425,18 @@ class Window(QWidget):
             return Qt.black
 
     def gameLoop(self):
+
+        if self.IteratorTimer >= 5 and (not self.isGameOnline and not self.isEnemyTurn):
+            self.createXML()
+            self.IteratorTimer = 0
+        else:
+            self.IteratorTimer += 1
         if self.isGameOnline and self.shouldcreateRoom:
             self.shouldcreateRoom = False
             self.createRoom()
         if not self.isEnemyTurn and self.canIMove:
             isSettling, isPlayer1, score = self.engine.game()
+
             if isSettling:
                 self.timer.setInterval(600)
                 if self.isGameOnline:
